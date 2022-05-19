37027a05f657febb9c08e3befe1360813aaa067c
marisoldazaa <marisol.dazaa@gmail.com>
2022-05-19 17:24:50 -0500

    Se hace la instalación de hooks para formulacio. Se crea un formulario con los hooks y también se logra que en consola se capturen los datos cuando se envia el formulario y se crea una llave con acumulado, pero en esta llave solo se se puede ver las modificaciones de la descripción

--- lab-notes/src/view/Notes.js
+++ lab-notes/src/view/Notes.js
@@ -1,160 +1,66 @@
-import React, { useState } from "react";
- import { useNavigate } from "react-router-dom";
+import React, { Fragment, useState } from "react";
+/*  import { useNavigate } from "react-router-dom";
 import { collection, addDoc, getFirestore } from "firebase/firestore";
-import {db, saveNote, getNote, onGetNote} from "../controler/firebase-init"
-import { async } from "@firebase/util"; 
+import {db, saveNote, getNote, onGetNote} from "../controler/firebase-init" */
+import { async } from "@firebase/util";
+import { useForm, Controller } from "react-hook-form";
 
+const HookForm = () => {
+  const { register, errors, control, handleSubmit } = useForm();
 
-function Notes(){
+  const [datos, setDatos] = useState([""]);
 
-  const [datos, setDatos] = useState({
-    title: '',
-    description: '',
-})
-  const handleInputChange = (event) => {
-   /*  console.log(event.target.name)
-    console.log(event.target.value)  */
-   setDatos({
-    title: document.getElementById('title-notes').value,
-    description: document.getElementById('notes-description').value,
-     /* ...datos,  */
-     [event.target.name] : event.target.value,
+  const saveData = (data) => {
+    console.log(data);
+         setDatos([
+      ...datos,
+       data
        
-    }) 
-  } 
-  const sendData = (event) => {
-    /* event.preventDefault(); */
-    console.log(event)
-    console.log('enviando datos...' + datos.title + ' ' + datos.description)
-  
-  } 
- 
- /*<form onSubmit={ev => {ev.preventDefault();console.log(ev.target.setInfo.value);}}></form> */
-  
-  return(
-    <div>
-      <h1> Estamos en notitas ¿ok? </h1>
-        <form /* onSubmit={sendData} */>
-      <div name="dataForm">  
-        <div >
-          <input type="text" 
-           placeholder="Titulo" 
-           id="title-notes" 
-           className="title-notes" 
-           onChange={handleInputChange}  
-           name="title-notes" 
-           />
-        </div>
-
-        <div>
-           <input type="text" 
-           placeholder="Descripción" 
-           id="notes-description" 
-           className="notes-description" 
-           onChange={handleInputChange} 
-           name="notes-description"
-           />
-        </div>
-       </div>
-        <div>
-          <button type="button" onClick={sendData}  className="notes-button">Guardar</button>
-        </div>
-      </form>
-      <h3>{datos.title} - {datos.description}</h3> 
-    </div>
-  )
-}
-export default Notes;
-
-/* const Create = () =>{
-  const [description, setDescription] = useState ('')
-  const [stock, setstock] = useState(0)
-  const navigate = useNavigate()
-
-  const notesCollection = collection(db, "notes")
-  
-  const store = async(e) =>{
-    e.preventDefault ()
-    await addDoc(notesCollection, {description: description, stock: stock })
-  }
-  
+  ]) 
+    /*   e.target.reset();  // limpiar campos  */
+  };
   return (
-    <div className="contairner">
-       <div className= "row">
-        <div className= "col">
-          <h1>Create Product</h1>
+    <Fragment>
+      <h2>Este formulario esta hecho con hook</h2>
+      <form onSubmit={handleSubmit(saveData)}>
+        <Controller
+          render={({ field: { onChange, onBlur, value, ref } }) => (
+            <input
+              placeholder="Titulo"
+              className="titleNotes"
+              onChange={onChange}
+              /* onBlur={onBlur} */
+              /* selected={value} */
+            />
+          )}
+          control={control}
+          name="title"
+        />
 
-          <form onSubmit={store}>
-            <div className= "mb-3">
-              <label className="form-label"> Description</label>
-              <input
-                value={description}
-                onChange={(e) => setDescription(e.target.value)}
-                type= "text"
-                className="form-control"
-              />
-            </div>
-            <div className= "mb-3">
-              <label className="form-label">Stock</label>
-              <input
-                value={stock}
-                onChange={(e) => setDescription(e.target.value)}
-                type= "text"
-                className="form-control"
-              />
-            </div>
+        <Controller
+          render={({ field: { onChange, onBlur, value, ref } }) => (
+            <input
+              placeholder="Descripcion"
+              className="descripcionNotes"
+              onChange={onChange}
+            />
+          )}
+          control={control}
+          name="description"
+        />
+        <button type="submit" className="btn-notas-primary">
+          Publicar
+        </button>
+      </form>
+      <ul className="list">
+        {datos.map((value, index) => (
+          <li key={index}>
+            {value.titulo} - {value.description}
+          </li>
+        ))}
+      </ul>
+    </Fragment>
+  );
+};
 
-          </form>
-      </div>
-      </div>
-      </div>
-  )
-} 
-
-export default function Notes(){
-  const Create = () =>{
- 
-  onGetNote((querySnapshot) => {
-    let noteList = "";
-    querySnapshot.forEach((doc) => {
-      const notes = doc.data();
-      noteList += 
-      
-      <h1>Hallo </h1>
-    }
-    )
-  }
-  )    
-   return (
-    <div>
-      <h1>Pon tus notitas aquí </h1>
-
-     {  <form onSubmit={store}>  }
-      <div className="container">
-        <div className="row">
-          <div className="col">
-            <h1> Escribe para no perder ninguna idea</h1>
-             
-            <div className="mb-3">
-              <label className="form-label"> Description</label>
-              <input type="text" className="form-control" 
-              value={description}
-              onChange={(e) => setDescription(e.target.value)} 
-                />
-            </div>
-
-            <div className="mb-3">
-              <label className="form-label">Stock</label>
-              <input type="text" className="form-control" />
-            </div>
-          </div>
-        </div>
-        
-      </div>
-     { </form> }
-    </div>
-    );
-   
-  }
-  return Create (); 
-} */   
+export default HookForm;
