9d2f2039160d276a3a3a8927d9f5421297453cb3
Carolina Preciado <carolinapreciadoleal@gmail.com>
2022-05-24 16:57:42 -0500

    Creación de vistas de login y home, ruteo con angular close #3

--- lab-notes/src/app/app-routing.module.ts
+++ lab-notes/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './views/home/home.component';
+import { LoginComponent } from './views/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LoginComponent
+  },
+  {
+    path: 'post/:id',
+    component: HomeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- lab-notes/src/app/app.module.ts
+++ lab-notes/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './views/login/login.component';
+import { HomeComponent } from './views/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
